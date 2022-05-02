5ae9e3c761cfb574c2dc1f41d7f766fa75da65b1
atclark <adam.tclark@gmail.com>
2022-05-02 16:51:38 +0200

    updated to run SD card on mkr 1310

--- receiver_code/receiver_code.ino
+++ receiver_code/receiver_code.ino
@@ -40,15 +40,16 @@ int tmp_time = 99; // initialize outside of plausible range
 // set up SD card
 /*
    SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+ ** MOSI/COPI - pin 8
+ ** MISO/CIPO - pin 10
+ ** SCK - pin 9
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN) TODO: potentially try changing to 5?
 
   modified from David A. Mellis (Nov 2010) and Tom Igoe (Apr 2012)
 */
 #include <SD.h>
 File myFile;
+int CS_pin = 4;
 
 void setup() {
   Serial.begin(9600);
@@ -59,6 +60,16 @@ void setup() {
 
   LoRa.begin(freq); // Set LoRa frequency
   LoRa.setSpreadingFactor(SF);  // TODO: think about best SF value
+
+  // Set up SD
+  // TODO: Move into loop if necessary?
+  Serial.print("SD Initializing SD card...");
+  if (!SD.begin(CS_pin)) {
+    Serial.println("initialization failed!");
+  }
+  else {
+    Serial.println("initialization done.");
+  }
 }
 
 void loop() {
@@ -178,7 +189,7 @@ void loop() {
                 delay(random(send_interval/10));
               }
               signal_received = 1;
-              trans_position = (trans_position+1) % nrecords; // avoid buffer overflow
+              trans_position = (trans_position+1); // TODO: re-add % nrecords to avoid buffer overflow?
             }
             else {
               Serial.print("Checksum failed. ");
@@ -210,67 +221,58 @@ void loop() {
     // Save to SD card
     if(trans_position>0) {
       // some data sucessfully transmitted
-      pinMode(LORA_RESET, OUTPUT);  //LORA reset pin declaration as output
-      digitalWrite(LORA_RESET, LOW);  //turn off LORA module
-      Serial.begin(9600);
-      while (!Serial) ; //wait for serial monitor to connect
-      delay(500);
+
+      //TODO: remove below if not needed
+      //pinMode(LORA_RESET, OUTPUT);  //LORA reset pin declaration as output
+      //digitalWrite(LORA_RESET, LOW);  //turn off LORA module
+      
+      // open the file. note that only one file can be open at a time,
+      // so you have to close this one before opening another.
+      int unit_number_log = save_unit_number[1];
+      myFile = SD.open("logfile_"+(String)unit_number_log+".csv", FILE_WRITE);
     
-      Serial.print("Initializing SD card...");
-      if (!SD.begin(32)) {
-        Serial.println("initialization failed!");
-      }
-      else {
-        Serial.println("initialization done.");
-      
-        // open the file. note that only one file can be open at a time,
-        // so you have to close this one before opening another.
-        int unit_number_log = save_unit_number[1];
-        myFile = SD.open("logfile_"+(String)unit_number_log+".csv", FILE_WRITE);
-      
-        // if the file opened okay, write to it:
-        if (myFile) {
-          Serial.print("Writing file...");
-      
-          // Create combined string line for data:
-          for(int j = 0; j < nrecords; j++) {
-            for(i = 0; i < air_probe_number; i++) {
-              myFile.print(save_air_temp[j][i]);
-              myFile.print("; ");
-            }
-            for(i = 0; i < air_probe_number; i++) {
-              myFile.print(save_air_humid[j][i]);
-              myFile.print("; ");
-            }
-            for(i = 0; i < soil_temp_probe_number; i++) {
-              myFile.print(save_soil_temp[j][i]);
-              myFile.print("; ");
-            }
-            for(i = 0; i < soil_mosit_probe_number; i++) {
-              myFile.print(save_soil_moist[j][i]);
-              myFile.print("; ");
-            }
-            myFile.print(save_battery[j]);
+      // if the file opened okay, write to it:
+      if (myFile) {
+        Serial.print("Writing file...");
+    
+        // Create combined string line for data:
+        for(int j = 0; j < nrecords; j++) {
+          for(i = 0; i < air_probe_number; i++) {
+            myFile.print(save_air_temp[j][i]);
             myFile.print("; ");
-            for(i = 0; i < 4; i++) {
-              myFile.print(save_time[j][i]);
-              myFile.print("; ");
-            }
-            myFile.print(save_rtc_temp[j]);
-            myFile.print("; ");
-            myFile.print(save_unit_number[j]);
-            myFile.print("; ");
-            myFile.print(save_checksum_out[j]);
-            myFile.println();
           }
-          
-          myFile.close();
-          Serial.println("Saving file done.");
-        } else {
-          // if the file didn't open, print an error:
-          Serial.println("Error opening file.");
+          for(i = 0; i < air_probe_number; i++) {
+            myFile.print(save_air_humid[j][i]);
+            myFile.print("; ");
+          }
+          for(i = 0; i < soil_temp_probe_number; i++) {
+            myFile.print(save_soil_temp[j][i]);
+            myFile.print("; ");
+          }
+          for(i = 0; i < soil_mosit_probe_number; i++) {
+            myFile.print(save_soil_moist[j][i]);
+            myFile.print("; ");
+          }
+          myFile.print(save_battery[j]);
+          myFile.print("; ");
+          for(i = 0; i < 4; i++) {
+            myFile.print(save_time[j][i]);
+            myFile.print("; ");
+          }
+          myFile.print(save_rtc_temp[j]);
+          myFile.print("; ");
+          myFile.print(save_unit_number[j]);
+          myFile.print("; ");
+          myFile.print(save_checksum_out[j]);
+          myFile.println();
         }
-      } 
+        
+        myFile.close();
+        Serial.println("Saving file done.");
+      } else {
+        // if the file didn't open, print an error:
+        Serial.println("Error opening file.");
+      }
     }
   
     Serial.println("End of transmission task.");
